138120a985c42733c80f5a0598cc235d45e205f6
wyongfei <wyongfei@163.com>
2017-02-08 17:17:22 +0800

    pthread
    Signed-off-by: wyongfei <wyongfei@163.com>

--- main.c
+++ main.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
-#include <json-c/json.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void *thrfun(void *arg){
+        sleep(1);//使得主线程先退出
+        printf("new thread\r\n");
+
+        printf("chile exit\r\n");
+
+        pthread_exit(0);
+}
+
 
 int main() {
-    printf("Hello, World!\n");
-    printf("Hello, World!\n");
-    printf("Hello, World!\n");
-    printf("Hello, World!\n");
-    return 0;
+        printf("Hello, World!\n");
+
+        pthread_t pid;
+
+        pthread_create(&pid,NULL,thrfun,NULL);
+
+        pthread_join(pid,NULL);
+
+        while (1){
+                sleep(3);
+
+                printf("father alive\r\n");
+        }
+
+        printf("father exit\r\n");
+
+        return 0;
 }
